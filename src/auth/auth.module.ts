52b80fc493aaf064290070e1ca2742328d9df75b
Ahmed Raouf <ahmedraouf.metwally@gmail.com>
2025-02-03 11:06:20 +0200

    add auth helper and update auth module for user creation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,15 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthHelper } from "./helper/auth.helper";
 
 @Module({
     controllers: [
         AuthController
     ],
     providers: [
-        AuthService
+        AuthService,
+        AuthHelper
     ],
 })
 export class AuthModule { }
